da76c77325e7da437b0c0784e769c768030fb078
Roberto Focke <robertofocke@users.noreply.github.com>
2017-10-02 13:45:44 -0300

    Add files via upload

---
+++ bin/Debug/Producto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class Producto
+    {
+        public string idProducto{get; set;}
+        public string producto { get; set; }
+        public int cantidad { get; set; }
+        public double precioCosto { get; set; }
+        public double precioVenta { get; set; }
+
+        public Producto(string idProducto, string producto, int cantidad, double precioCosto, double precioVenta) {
+            idProducto = this.idProducto;
+            producto = this.producto;
+            cantidad = this.cantidad;
+            precioCosto = this.precioCosto;
+            precioVenta = this.precioVenta;
+
+        }
+
+        public string getProducto() {
+            return this.idProducto;
+        }
+    }
+}
